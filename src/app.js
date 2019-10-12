0d2ec1638cc85aa8f55b92eca266736c61759e35
laura <laura.j.elias@gmail.com>
2019-10-11 21:43:21 -0500

    server ready

--- src/app.js
+++ src/app.js
@@ -4,15 +4,19 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config.js');
+const nRouter = require('./notes/notes-router.js');
+const fRouter = require('./folders/folders-router.js');
+
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
-
-app.use(morgan(morganOption));
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'}));
 app.use(helmet());
 app.use(cors());
 
+app.use('/api/notes', nRouter);
+app.use('/api/folders', fRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, noteful users!');
